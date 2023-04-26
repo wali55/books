8f9cd1ccf1d260acd4ddfc3ea918a11113f4c6aa
wali55 <walisantunu@gmail.com>
2023-04-26 16:54:37 +0600

    add a book

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,17 @@ function App() {
     const [books, setBooks] = useState([]);
 
     const createBook = (title) => {
-        console.log('Add book with:', title)
+        const updatedBooks = [
+            ...books,
+            { id: 123, title }
+        ];
+
+        setBooks(updatedBooks);
     };
 
     return (
     <div>
+        {books.length}
         <BookCreate onCreate={createBook} />
     </div>
     );
