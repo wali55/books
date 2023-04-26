15e69a73ead969ad9ddd2c4ebcfd07c9214eb4af
wali55 <walisantunu@gmail.com>
2023-04-26 20:02:22 +0600

    delete records

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
 import { useState } from 'react';
 import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
 
 function App() {
     const [books, setBooks] = useState([]);
 
+    const deleteBookById = (id) => {
+        const updatedBooks = books.filter((book) => {
+            return book.id !== id;
+        });
+
+        setBooks(updatedBooks);
+    };
+
     const createBook = (title) => {
         const updatedBooks = [
             ...books,
-            { id: 123, title }
+            { 
+                id: Math.round(Math.random() * 9999), 
+                title 
+            }
         ];
 
         setBooks(updatedBooks);
     };
 
     return (
-    <div>
-        {books.length}
+    <div className="app">
+        <BookList books={books} onDelete={deleteBookById} />
         <BookCreate onCreate={createBook} />
     </div>
     );
