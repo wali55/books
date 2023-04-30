cd9511d98f7946f0b38b0042838617a061fb30a0
wali55 <walisantunu@gmail.com>
2023-04-30 21:41:05 +0600

    combine handlers

--- src/components/BookShow.jsx
+++ src/components/BookShow.jsx
@@ -12,9 +12,14 @@ function BookShow({ book, onDelete, onEdit }) {
         setShowEdit(!showEdit);
     };
 
+    const handleSubmit = (id, newTitle) => {
+        setShowEdit(false);
+        onEdit(id, newTitle);
+    };
+
     let content = <h3>{book.title}</h3>;
     if (showEdit) {
-        content = <BookEdit book={book} onEdit={onEdit} />;
+        content = <BookEdit onSubmit={handleSubmit} book={book} />;
     }
 
     return (
